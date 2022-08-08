4e13646469b478a23220b94b714ef73f454ab49f
Aaron Webster <awebster@gmail.com>
2022-08-08 11:39:11 -0700

    Make version date based to clean up commit history.

--- .github/scripts/bump_version.py
+++ .github/scripts/bump_version.py
@@ -1,25 +1,5 @@
+import datetime
 
-major = None
-minor = None
-patch = None
-
-with open('VERSION', 'r') as f:
-  major, minor, patch = [int(x) for x in f.read().strip().split('.')]
-
-# Arbitrary increment process.
-
-patch = patch + 1
-
-if patch > 20:
-  patch = 0
-  minor = minor + 1
-
-if minor > 20:
-  minor = 0
-  major = major + 1
-
-version = '{}.{}.{}'.format(major,minor,patch)
-with open('VERSION', 'w') as f:
-  f.write(version)
-
+d = datetime.datetime.now(datetime.timezone.utc)
+version = d.strftime('%Y.%j.%H%M%S')
 print('::set-output name=version::v{}'.format(version))
